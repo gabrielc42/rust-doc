ef8ed284b192f917c4d63e5cd491e89327e3c7c7
gabrielc42 <clark.gabe97@gmail.com>
2022-12-13 19:28:18 -0600

    functions, comments && loops and conditions

---
+++ functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\Gabriel\Desktop\reposit\rust-doc\functions\target\debug\deps\functions.exe: src\main.rs
+
+C:\Users\Gabriel\Desktop\reposit\rust-doc\functions\target\debug\deps\functions.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\Gabriel\Desktop\reposit\rust-doc\functions\target\debug\functions.exe: C:\Users\Gabriel\Desktop\reposit\rust-doc\functions\src\main.rs
