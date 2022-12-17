5f0c5caf28dcf7effab86b5cf9a2be396523847b
gabrielc42 <clark.gabe97@gmail.com>
2022-12-16 19:53:54 -0600

    structs

---
+++ structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+C:\Users\Gabriel\Desktop\reposit\rust-doc\structs\target\debug\deps\structs.exe: src\main.rs
+
+C:\Users\Gabriel\Desktop\reposit\rust-doc\structs\target\debug\deps\structs.d: src\main.rs
+
+src\main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+C:\Users\Gabriel\Desktop\reposit\rust-doc\structs\target\debug\structs.exe: C:\Users\Gabriel\Desktop\reposit\rust-doc\structs\src\main.rs
