ac87dcd4e74c8a1e5ee5ad213519c0d89abe34ed
gabrielc42 <clark.gabe97@gmail.com>
2022-12-16 20:03:43 -0600

    method syntax

---
+++ methods/target/debug/deps/methods-735b18df60b36f84.d
@@ -0,0 +1,5 @@
+c:\Users\Gabriel\Desktop\reposit\rust-doc\methods\target\debug\deps\methods-735b18df60b36f84.rmeta: src\main.rs
+
+c:\Users\Gabriel\Desktop\reposit\rust-doc\methods\target\debug\deps\methods-735b18df60b36f84.d: src\main.rs
+
+src\main.rs:
