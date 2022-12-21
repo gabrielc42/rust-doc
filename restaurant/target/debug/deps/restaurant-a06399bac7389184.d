4d3d3c096d57b89c628300f588bec9df6ec58e4d
gabrielc42 <clark.gabe97@gmail.com>
2022-12-20 19:58:47 -0600

    modules seperated, absolute paths and relative paths, pub use: all kind of confusing but when a project gets to this point this will not be a problem

---
+++ restaurant/target/debug/deps/restaurant-a06399bac7389184.d
@@ -0,0 +1,7 @@
+C:\Users\Gabriel\Desktop\reposit\rust-doc\restaurant\target\debug\deps\restaurant-a06399bac7389184.rmeta: src\lib.rs
+
+C:\Users\Gabriel\Desktop\reposit\rust-doc\restaurant\target\debug\deps\librestaurant-a06399bac7389184.rlib: src\lib.rs
+
+C:\Users\Gabriel\Desktop\reposit\rust-doc\restaurant\target\debug\deps\restaurant-a06399bac7389184.d: src\lib.rs
+
+src\lib.rs:
