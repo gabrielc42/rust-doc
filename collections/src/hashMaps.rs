5d00459ce10d6591c403aa875aa6684f9e90d983
gabrielc42 <clark.gabe97@gmail.com>
2022-12-23 12:38:37 -0600

    hashMaps lesson, exercises beginning

--- collections/src/hashMaps.rs
+++ collections/src/hashMaps.rs
@@ -5,4 +5,41 @@ pub fn hashMap() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25); // overwrites 10 to 25
+
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
--- collections/target/debug/deps/collections.d
+++ collections/target/debug/deps/collections.d
@@ -1,7 +1,8 @@
-C:\Users\Gabriel\Desktop\reposit\rust-doc\collections\target\debug\deps\collections.exe: src\main.rs src\string.rs src\vec.rs
+C:\Users\Gabriel\Desktop\reposit\rust-doc\collections\target\debug\deps\collections.exe: src\main.rs src\hashMaps.rs src\string.rs src\vec.rs
 
-C:\Users\Gabriel\Desktop\reposit\rust-doc\collections\target\debug\deps\collections.d: src\main.rs src\string.rs src\vec.rs
+C:\Users\Gabriel\Desktop\reposit\rust-doc\collections\target\debug\deps\collections.d: src\main.rs src\hashMaps.rs src\string.rs src\vec.rs
 
 src\main.rs:
+src\hashMaps.rs:
 src\string.rs:
 src\vec.rs:
