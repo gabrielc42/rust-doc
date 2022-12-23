d96995b2cb95b43de7e2ead618e1a9b05d2d7109
gabrielc42 <clark.gabe97@gmail.com>
2022-12-23 16:08:29 -0600

    errors halfway

---
+++ errors/target/debug/deps/errors-5e5cef4cb731464a.d
@@ -0,0 +1,5 @@
+c:\Users\Gabriel\Desktop\reposit\rust-doc\errors\target\debug\deps\errors-5e5cef4cb731464a.rmeta: src\main.rs
+
+c:\Users\Gabriel\Desktop\reposit\rust-doc\errors\target\debug\deps\errors-5e5cef4cb731464a.d: src\main.rs
+
+src\main.rs:
---
+++ errors/target/debug/deps/errors.d
@@ -0,0 +1,5 @@
+C:\Users\Gabriel\Desktop\reposit\rust-doc\errors\target\debug\deps\errors.exe: src\main.rs
+
+C:\Users\Gabriel\Desktop\reposit\rust-doc\errors\target\debug\deps\errors.d: src\main.rs
+
+src\main.rs:
