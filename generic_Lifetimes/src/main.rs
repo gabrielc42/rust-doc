3cd08531b3acc386e2ee8e27333ba13da93e13e5
gabrielc42 <clark.gabe97@gmail.com>
2022-12-25 18:01:15 -0600

    generic types different from struct def

--- generic_Lifetimes/src/main.rs
+++ generic_Lifetimes/src/main.rs
@@ -1,6 +1,15 @@
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 fn largest<T>(list: &[T]) -> &T {
@@ -28,4 +37,11 @@ fn main() {
 
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
