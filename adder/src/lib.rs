ec5618a0991651bb22bc20eeeec46633955c39ef
gabrielc42 <clark.gabe97@gmail.com>
2023-01-07 21:23:51 -0600

    custom test failure messages, assert_eq & assert_ne asserts

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -5,8 +5,24 @@
 // This allows for integration tests written against the library,
 // which might otherwise be hard to do with only main.rs.
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
@@ -28,4 +44,52 @@ mod tests {
     fn another() {
         panic!("Make this test fail");
     }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2)); //We pass 4 as the argument to assert_eq!, which is equal to the result of calling add_two(2)
+                                   //assertion failed: `(left == right)` if not ==
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello !")
 }
