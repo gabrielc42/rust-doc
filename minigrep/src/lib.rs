a2de85f2f5e63e1425a276fa94a239f23d07e044
gabrielc42 <clark.gabe97@gmail.com>
2023-01-15 17:49:59 -0600

    search fn doesn't find by word?

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -7,12 +7,11 @@ pub struct Config {
 }
 
 impl Config {
-    // change the function name from new to build
-    // because many programmers expect new functions to never fail
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
@@ -23,7 +22,37 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
