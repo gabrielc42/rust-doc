31b0d1ed7c5afe738a7c92e43dbcb967456c3213
gabrielc42 <clark.gabe97@gmail.com>
2023-01-17 15:09:48 -0600

    eprintln, output.txt with searched keywords

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     //dbg!(args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
