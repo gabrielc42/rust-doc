c1aaacfbfec1e00ace0f73cf92ad0471779126fb
gabrielc42 <clark.gabe97@gmail.com>
2023-01-22 16:34:41 -0600

    closures: anonymous fn that capture env

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -54,6 +54,99 @@ fn main() {
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    // let example_closure = |x| x;
+
+    // let s = example_closure(String::from("hello"));
+    // let n = example_closure(5);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
+
+    // let mut list = [
+    //     Rectangle {
+    //         width: 10,
+    //         height: 1,
+    //     },
+    //     Rectangle {
+    //         width: 3,
+    //         height: 5,
+    //     },
+    //     Rectangle {
+    //         width: 7,
+    //         height: 12,
+    //     },
+    // ];
+
+    // let mut sort_operations = vec![];
+    // let value = String::from("by key called");
+
+    // list.sort_by_key(|r| {
+    //     sort_operations.push(value);
+    //     r.width
+    // });
+    // println!("{:#?}", list);
+
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
 }
 
 use std::thread;
@@ -76,4 +169,15 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!("Today, run for {} minutes!", expensive_closure(intensity));
         }
     }
+}
+
+// fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+// let add_one_v2 = |x: u32| -> u32 { x + 1 };
+// let add_one_v3 = |x|             { x + 1 };
+// let add_one_v4 = |x|               x + 1  ;
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
