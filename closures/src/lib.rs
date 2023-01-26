1113ee1767c10151dd519f94ddeea59cc6d92550
gabrielc42 <clark.gabe97@gmail.com>
2023-01-26 05:11:43 -0600

    processing series of items w/ iterators

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,6 +1,17 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
+    // v1_iter mut, calling next on iterator changes internal state
     fn iterator_demonstration() {
         let v1 = vec![1, 2, 3];
 
@@ -10,5 +21,51 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&2));
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    // We aren’t allowed to use v1_iter
+    // after the call to sum because
+    // sum takes ownership of the iterator we call it on.
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
+
+    use super::*;
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
     }
 }
