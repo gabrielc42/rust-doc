644b8647ed73d02ca16ed84fc52b21dfc0a069b0
gabrielc42 <clark.gabe97@gmail.com>
2023-01-27 22:05:44 -0600

    io project: fn search iterator, Config build args.next to mquery & file match, main.rs env::args() (returns iterator) rather than pass args ref

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,18 +4,25 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
+    // let args: Vec<String> = env::args().collect();
+    // //dbg!(args);
+
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
+    // if let Err(e) = minigrep::run(config) {
+    //     println!("Application Error: {e}");
+    //     process::exit(1);
+    // }
+
     let args: Vec<String> = env::args().collect();
-    //dbg!(args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
-    });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-    if let Err(e) = minigrep::run(config) {
-        println!("Application Error: {e}");
-        process::exit(1);
-    }
+    })
 }
