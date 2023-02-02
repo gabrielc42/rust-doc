b8ef507eec20140f77ad4973992ee4dd601f5786
gabrielc42 <clark.gabe97@gmail.com>
2023-02-02 06:02:12 -0600

    smart_pointers deref trait

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -5,9 +5,53 @@ enum List {
 
 use crate::List::{Cons, Nil};
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let b = Box::new(5);
     println!("b = {}", b);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&(*m)[..]);
 }
+
+//Rust does deref coercion when it finds types and trait implementations in three cases:
+// From &T to &U when T: Deref<Target=U>
+// From &mut T to &mut U when T: DerefMut<Target=U>
+// From &mut T to &U when T: Deref<Target=U>
+
+// immutable references will never coerce to mutable references
+// Converting an immutable reference to
+// a mutable reference would require that the initial immutable reference is
+// the only immutable reference to that data, but the borrowing rules don’t guarantee that
