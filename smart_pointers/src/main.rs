e13b9d26d424ad9e05ffbdc07d72302e88b04dfe
gabrielc42 <clark.gabe97@gmail.com>
2023-02-08 08:41:35 -0600

    reference cycles leaking memory, tree data structure: -> from child to parent, changes to strong_count and weak_count

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)]
+
 // enum List {
 //     Cons(i32, Box<List>),
 //     Nil,
@@ -15,7 +17,9 @@
 // }
 
 use crate::List::{Cons, Nil};
-use std::rc::Rc;
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::{Rc, Weak};
 
 struct MyBox<T>(T);
 
@@ -24,8 +28,6 @@ impl<T> MyBox<T> {
         MyBox(x)
     }
 }
-
-use std::ops::Deref;
 
 impl<T> Deref for MyBox<T> {
     type Target = T;
@@ -49,15 +51,40 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+// #[derive(Debug)]
+// enum List {
+//     Cons(Rc<RefCell<i32>>, Rc<List>),
+//     Nil,
+// }
+
 #[derive(Debug)]
 enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
-// use crate::List::{Cons, Nil};
-use std::cell::RefCell;
-// use std::rc::Rc;
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+// #[derive(Debug)]
+// struct Node {
+//     value: i32,
+//     children: RefCell<Vec<Rc<Node>>>,
+// }
+
+// ref from child to parent
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
 
 fn main() {
     // box<t> and deref trait
@@ -118,18 +145,113 @@ fn main() {
     // the same place can cause data races and inconsistencies
 
     //multiple owners of mutable data by combining Rc<T> and RefCell<T>
-    let value = Rc::new(RefCell::new(5));
+    // let value = Rc::new(RefCell::new(5));
 
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    // let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
 
-    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    // let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    // let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
 
-    *value.borrow_mut() += 10;
+    // *value.borrow_mut() += 10;
 
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+    // println!("a after = {:?}", a);
+    // println!("b after = {:?}", b);
+    // println!("c after = {:?}", c);
+
+    //reference cycles leaking memory
+    // let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    // println!("a initial rc count = {}", Rc::strong_count(&a));
+    // println!("a next item = {:?}", a.tail());
+
+    // let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    // println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    // println!("b initial rc count = {}", Rc::strong_count(&b));
+    // println!("b next item = {:?}", b.tail());
+
+    // if let Some(link) = a.tail() {
+    //     *link.borrow_mut() = Rc::clone(&b);
+    // }
+
+    // println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    // println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // // Uncomment the next line to see that we have a cycle;
+    // // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
+
+    //tree data structure
+    // let leaf = Rc::new(Node {
+    //     value: 3,
+    //     children: RefCell::new(vec![]),
+    // });
+
+    // let branch = Rc::new(Node {
+    //     value: 5,
+    //     children: RefCell::new(vec![Rc::clone(&leaf)]),
+    // });
+
+    //from child to parent
+    // let leaf = Rc::new(Node {
+    //     value: 3,
+    //     parent: RefCell::new(Weak::new()),
+    //     children: RefCell::new(vec![]),
+    // });
+
+    // println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    // let branch = Rc::new(Node {
+    //     value: 5,
+    //     parent: RefCell::new(Weak::new()),
+    //     children: RefCell::new(vec![Rc::clone(&leaf)]),
+    // });
+
+    // *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    // println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    //changes to strong_count and weak_count
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
 
 //Rust does deref coercion when it finds types and trait implementations in three cases:
@@ -148,3 +270,8 @@ fn main() {
 // Rc<T> allows only immutable borrows checked at compile time;
 // RefCell<T> allows immutable or mutable borrows checked at runtime.
 // Because RefCell<T> allows mutable borrows checked at runtime, you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+
+// automated tests, code reviews, and other software development practices to minimize reference cycles
+// also, reorganizing your data structures so that some references express ownership and some references don’t.
+// As a result, you can have cycles made up of some ownership relationships and some non-ownership relationships,
+// and only the ownership relationships affect whether or not a value can be dropped.
