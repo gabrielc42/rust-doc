06ffed0cccc05efc20cb097e3c6d336a511c603d
gabrielc42 <clark.gabe97@gmail.com>
2023-02-09 21:48:43 -0600

    sharing mutex<T> between multiple threads

--- concurrency/src/mutex.rs
+++ concurrency/src/mutex.rs
@@ -1,4 +1,4 @@
-use std::sync::Mutex;
+use std::{sync::Mutex, thread};
 
 pub fn mutex() {
     // Mutex<T> is a smart pointer
@@ -10,4 +10,22 @@ pub fn mutex() {
     }
 
     println!("m = {:?}", m);
+
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
