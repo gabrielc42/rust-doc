277963c8956438a2a60871bfd7d74642285501a6
gabrielc42 <clark.gabe97@gmail.com>
2023-02-09 21:03:43 -0600

    multi producers by cloning transmitter

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -42,12 +42,28 @@ fn main() {
     //     // will not compile
     // });
 
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
             String::from("from"),
             String::from("the"),
             String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
         ];
 
         for val in vals {
