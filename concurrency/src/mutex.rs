fba68f6496e03f6c6fb0f9f75af4002cdb1a3aaa
gabrielc42 <clark.gabe97@gmail.com>
2023-02-09 21:51:14 -0600

    Rc compile errors for multi ownership w/ multi threads

--- concurrency/src/mutex.rs
+++ concurrency/src/mutex.rs
@@ -1,4 +1,4 @@
-use std::{sync::Mutex, thread};
+use std::{rc::Rc, sync::Mutex, thread};
 
 pub fn mutex() {
     // Mutex<T> is a smart pointer
@@ -11,10 +11,11 @@ pub fn mutex() {
 
     println!("m = {:?}", m);
 
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Rc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
