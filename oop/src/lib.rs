1d1c9f7bac19eedc7caae1f50e978bd25d125b7e
gabrielc42 <clark.gabe97@gmail.com>
2023-02-10 21:40:03 -0600

    trait objects that allow for values of different types

--- oop/src/lib.rs
+++ oop/src/lib.rs
@@ -1,31 +1,79 @@
-pub Struct AveragedCollection {
-  list: Vec<i32>,
-  average: f64,
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {}
+}
+
+impl Draw for String {
+    fn draw(&self) {}
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// A generic type parameter can only be substituted with one concrete type at a time,
+// whereas trait objects allow for multiple concrete types to fill in for the trait object at runtime
+
+// pub struct Screen<T: Draw> {
+//   pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//   T: Draw,
+// {
+//   pub fn run(&self) {
+//       for component in self.components.iter() {
+//           component.draw();
+//       }
+//   }
+// }
+
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
 }
 
 impl AveragedCollection {
-  pub fn add(&mut self, value: i32) {
-    self.list.push(value);
-    self.update_average();
-  }
-
-  pub fn remove(&mut self) -> Option<i32> {
-    let result = self.list.pop();
-    match result {
-      Some(value) => {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
         self.update_average();
-        Some(value)
-      }
-      None => None,
     }
-  }
 
-  pub fn average(&self) -> f64 {
-    self.average
-  }
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
 
-  fn update_average(&mut self) {
-    let total: i32 = self.list.iter().sum();
-    self.average = total as f64 / self.list.len() as f64;
-  }
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
 }
--- oop/src/main.rs
+++ oop/src/main.rs
@@ -1,3 +1,39 @@
+use oop::Draw;
+use oop::{Button, Screen};
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![Box::new(String::from("Hi"))], // passing something wrong
+                                                        // implement Draw on String so that Screen is able to call Draw
+    };
+    screen.run();
+
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+    screen.run();
 }
