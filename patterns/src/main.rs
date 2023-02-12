c62362cf61fc236efb25eb46141676a9b70f2dc7
gabrielc42 <clark.gabe97@gmail.com>
2023-02-12 02:30:24 -0600

    pattern syntax before destructuring section

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -52,6 +52,56 @@ fn main() {
     if let x = 5 {
         println! {"{}", x};
     }; // is refutable and suggests 'let' only
+
+    // matching literals
+
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // matching named vars
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+    // will be a new y var, binding to inner value of Some in x
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // multiple patterns
+
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // matching ranges of values ..=
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    // range char values
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("somethin' else i reckon..."),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
