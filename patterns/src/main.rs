5ef393254622ed60bf348595cebef95fc010dcdc
gabrielc42 <clark.gabe97@gmail.com>
2023-02-12 02:10:40 -0600

    all places patterns can be used

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // if let, else if, else if let, else
     let favorite_color: Option<&str> = None;
     let is_tuesday = false;
     let age: Result<u8, _> = "25".parse();
@@ -16,4 +17,33 @@ fn main() {
     } else {
         println!("Blue as the background color");
     }
+
+    // while let
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    // for
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    // let
+    let (x, y, z) = (1, 2, 3); // left right amounts must be same
+
+    // function params
+    let point = (2, 3);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println! {"Current location: ({}, {})", x, y};
 }
