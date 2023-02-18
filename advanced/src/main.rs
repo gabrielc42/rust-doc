5b27c2c79245573d7801a2f7d9f0bb7ab6dbb57b
gabrielc42 <clark.gabe97@gmail.com>
2023-02-17 21:00:09 -0600

    advanced rust: unsafe -> dereferencing raw pointers within unsafe block listing 19-3

---
+++ advanced/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // unsafe
+    // dereference a raw pointer
+    // call an unsafe function or method
+    // access or modify a mutable static variable
+    // implement an unsafe trait
+    // access fields of union
+
+    // raw pointers
+    // are allowed to ignore borrowing rules by having
+    // both immutable and mutable pointers or
+    // multiple mutable pointers to same location
+    // aren't guaranteed to point to valid memory
+    // are allowed to be null
+    // don't implement any automatic cleanup
+
+    // immutable and mutable raw pointer from references
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    // raw pointer to arbitrary location in memory
+    // usually no good reason to write code like this
+    // compiler might optimize the code so no memory access,
+    // or program might error w/ a segmentation fault
+    let address = 0x012345usize;
+    let r address as *const i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+    
+}
