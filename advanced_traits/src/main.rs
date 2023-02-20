1753c4e1f4417a82a1152cf2dbff78f35c1dcab1
gabrielc42 <clark.gabe97@gmail.com>
2023-02-20 05:54:45 -0600

    overloading operators

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 fn main() {
     println!("Hello, world of traits!");
 
@@ -11,4 +13,35 @@ fn main() {
     // ...in lib.rs...
 
     // default generic type parameters and operator overloading
+    // overloading + operator to add two Point instances together
+    use std::ops::Add;
+
+    #[derive(Debug, Copy, Clone, PartialEq)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    impl Add for Point {
+        type Output = Point;
+
+        fn add(self, other: Point) -> Point {
+            Point {
+                x: self.x + other.x,
+                y: self.y + other.y,
+            }
+        }
+    }
+
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    // default gerneric type within Add
+    trait Add<Rhs = Self> {
+        type Output;
+
+        fn add(self, rhs: Rhs) -> Self::Output;
+    }
 }
