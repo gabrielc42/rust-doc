bd7f25202aa9d870e96bbcac8fff135bc70eb3c1
gabrielc42 <clark.gabe97@gmail.com>
2023-02-21 02:25:35 -0600

    nevertype!

--- advanced_types/src/main.rs
+++ advanced_types/src/main.rs
@@ -63,4 +63,51 @@ fn main() {
         fn write_all(&mut self, buf: &[u8]) -> Result<()>;
         fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
     }
+
+    // never type ... type that never returns: !
+    fn bar() -> ! {
+        // --snip--
+        panic!();
+    }
+
+    // but why? recal listing 2-5, guessing number game
+    use rand::Rng;
+    use std::cmp::Ordering;
+    use std::io;
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        // --snip--
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        // --snip--
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
