7a10cfd2700ee17cb4589f30fecd9719c354bf5a
gabrielc42 <clark.gabe97@gmail.com>
2023-02-21 02:23:09 -0600

    creating type synonyms with type aliases

--- advanced_types/src/main.rs
+++ advanced_types/src/main.rs
@@ -1,7 +1,9 @@
+use std::fmt::{self, Error};
+
 fn main() {
     println!("Hello, world of types! ⚡🐛❄️🌊🔥🐲");
 
-    // new types hiding interal impl
+    // new types hiding internal impl
     // People type wrap a HashMap<i32, String> that
     // stores a person's ID assoc. w/ their name
 
@@ -10,4 +12,55 @@ fn main() {
     // code wouldn't need to know that we assign and i32 ID to names internally
 
     // creating type synonyms with type aliases
+
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
+
+    // too much
+    // let f: Box<dyn Fn() + Send + 'static> = Box::new(|| println!("hi"));
+
+    // fn takes_long_type(f: Box<dyn Fn() + Send + 'static>) {
+    //     // --snip--
+    // }
+
+    // fn returns_long_type() -> Box<dyn Fn() + Send + 'static> {
+    //     // --snip--
+    // }
+
+    // type alias
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi!"));
+
+    fn takes_long_type(f: Thunk) {
+        // ...
+    }
+
+    fn returns_long_type() -> Thunk {
+        Box::new(|| println!("hi!"))
+    }
+
+    // Result<T, E>
+
+    // pub trait Write {
+    //     fn write(&mut self, buf: &[u8]) -> Result<usize, Error>;
+    //     fn flush(&mut self) -> Result<(), Error>;
+
+    //     fn write_all(&mut self, buf: &[u8]) -> Result<(), Error>;
+    //     fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
+    // }
+
+    type Result<T> = std::result::Result<T, std::io::Error>;
+
+    pub trait Write {
+        fn write(&mut self, buf: &[u8]) -> Result<usize>;
+        fn flush(&mut self) -> Result<()>;
+
+        fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+        fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+    }
 }
