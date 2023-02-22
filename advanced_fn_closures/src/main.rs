97612487db6d414a3436131c4e3a3109d68f8d7e
gabrielc42 <clark.gabe97@gmail.com>
2023-02-22 08:05:11 -0600

    function pointers init

--- advanced_fn_closures/src/main.rs
+++ advanced_fn_closures/src/main.rs
@@ -1,3 +1,16 @@
+// function pointers
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world of functions and closures!");
+
+    // function pointers
+    let answer = do_twice(add_one, 5);
+    println!("The answer is: {}", answer);
 }
