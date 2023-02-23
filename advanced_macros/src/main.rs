ada78ec8facae257b3f986d2999bd94f03a5ce8b
gabrielc42 <clark.gabe97@gmail.com>
2023-02-23 01:43:33 -0600

    macros init, declarative and procedural

---
+++ advanced_macros/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world of macros! ❗❓❔❕");
+    // declarative macros w/ macro_rules!
+    // procedural macros
+    // 1. Custom #[derive]: specify code added with the derive attribute used on structs and enums
+    // 2. Attribute-like: define custom attributes usable on any item
+    // 3. Function-like: look like function calls but operate on the tokens specified as their argument
+}
