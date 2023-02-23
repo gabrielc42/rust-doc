53d76d8b2344f65e2c661e80a4fa813f17c2776d
gabrielc42 <clark.gabe97@gmail.com>
2023-02-23 01:38:18 -0600

    advanced functions and closures: reread

--- advanced_fn_closures/src/main.rs
+++ advanced_fn_closures/src/main.rs
@@ -13,4 +13,30 @@ fn main() {
     // function pointers
     let answer = do_twice(add_one, 5);
     println!("The answer is: {}", answer);
+
+    // closure defined inline or a named function
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+
+    let list_of_numberz = vec![1, 2, 3];
+    let list_of_stringz: Vec<String> = list_of_numberz.iter().map(ToString::to_string).collect();
+
+    // initializer functions as function pointers that implement closure traits
+    // specify initializer functions as arguments for methods that take closures
+    enum Status {
+        Value(u32),
+        Stop,
+    }
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+    // returning closures
+    // below doesn't compile
+    // fn returns_closure() -> dyn Fn(i32) -> i32 {
+    //     |x| x + 1
+    // }
+
+    fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+        Box::new(|x| x + 1)
+    }
 }
