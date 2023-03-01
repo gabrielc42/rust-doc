ac686c491ec1f723d2211b640cdf6b93702a170e
gabrielc42 <clark.gabe97@gmail.com>
2023-03-01 10:52:42 -0600

    output of macro call

--- advanced_macros/src/main.rs
+++ advanced_macros/src/main.rs
@@ -19,3 +19,11 @@ macro_rules! vec {
         }
     };
 }
+
+// {
+//     let mut temp_vec = Vec::new();
+//     temp_vec.push(1);
+//     temp_vec.push(2);
+//     temp_vec.push(3);
+//     temp_vec
+// }
